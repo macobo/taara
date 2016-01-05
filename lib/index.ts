53886aa2a6b122346c56686cdf0127a01b6999a9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2016-01-05 16:52:48 +0200

    Document some of the public API, allow users to configure where temporary files go.

--- lib/index.ts
+++ lib/index.ts
@@ -7,10 +7,16 @@ import * as nodefn from "when/node";
 import * as API from "./API";
 import {arrayify, streamToFile} from "./utils";
 
+/** Lists all snapshots stored in storageEngine to date. */
 export function listSnapshots(storageEngine: API.StorageEngine): Promise<Array<API.SnapshotIdentifier>> {
     return storageEngine.list();
 }
 
+/**
+ * Takes a snapshot of tables from database and stores it on S3.
+ * @return a promise metadata with the identifier, metadata and stats for
+ * this snapshot.
+ */
 export function storeSnapshot(
     tables: string|Array<string>,
     userMetadata: Object,
@@ -31,13 +37,20 @@ export function storeSnapshot(
         .then(() => storageEngine.saveMetadata(storedData));
 }
 
+/**
+ * Given an identifier (from `taara.listSnapshots`) this will restore the table.
+ * Uses temporary files - to configure what directory to use, see `taara.setTempDir`.
+ *
+ * @return a promise for metadata with the identifier, metadata and stats for
+ * this snapshot.
+ */
 export function restoreSnapshot(
     identifier: API.SnapshotIdentifier,
     storageEngine: API.StorageEngine,
     dbEngine: API.DatabaseEngine
 ): Promise<API.StorageMetadata> {
     var storageMetadata;
-    const dumpFileLocation = temp.path("taara-restore-");
+    const dumpFileLocation = temp.path(tempFileOptions);
     return storageEngine
         .loadMetadata(identifier)
         .tap((metadata) => { storageMetadata = metadata; })
@@ -48,6 +61,7 @@ export function restoreSnapshot(
         .finally(() => nodefn.call(fs.unlink, dumpFileLocation));
 }
 
+
 export function deleteSnapshot(
     identifier: API.SnapshotIdentifier,
     storageEngine: API.StorageEngine
@@ -55,4 +69,12 @@ export function deleteSnapshot(
     return storageEngine
         .deleteMetadata(identifier)
         .then(() => storageEngine.deleteSnapshot(identifier));
+}
+
+var tempFileOptions: temp.AffixOptions = {
+    prefix: "taara-restore-"
+}
+
+export function setTempDir(dirPath: string) {
+    tempFileOptions.dir = dirPath;
 }
